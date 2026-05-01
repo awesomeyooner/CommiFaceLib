f3456a554c1cdd37fd9161f96d23ea520c2aeaf8
Aaron Yoon <awesomeyooner@gmail.com>
2026-05-01 00:21:29 -0700

    started working on communication interface

---
+++ include/CommiFaceLib/interfaces/communication_interface.hpp
@@ -0,0 +1,20 @@
+#ifndef COMMUNICATION_INTERFACE_HPP
+#define COMMUNICATION_INTERFACE_HPP
+
+
+#include "plib/util/status.hpp"
+
+
+class CommunicationInterface
+{
+
+    public:
+        
+
+    private:
+
+    
+
+}; // class CommunicationInterface
+
+#endif // COMMUNICATION_INTERFACE_HPP
