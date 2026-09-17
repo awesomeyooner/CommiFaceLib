a25311049bb4868bf5118bd8d56f4f9a90d594a5
Aaron Yoon <awesomeyooner@gmail.com>
2026-09-16 20:36:45 -0700

    Started working on canbus

---
+++ include/CommiFaceLib/protocols/can.hpp
@@ -0,0 +1,16 @@
+#ifndef CAN_HPP
+#define CAN_HPP
+
+
+#include "CommiFaceLib/interfaces/communication_interface.hpp"
+
+#include <linux/can.h>
+
+
+class CAN : public CommunicationInterface
+{
+
+}; // class CAN : public CommunicationInterface
+
+
+#endif // CAN_HPP
